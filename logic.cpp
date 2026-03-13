14f76723d5f1f5fbec158c083f544560f24984fd
Iva Jefremova <jefremovaiva17@gmail.com>
2026-03-13 14:04:50 +0100

    added two methods to my class Task, addTask and displayTasks

--- logic.cpp
+++ logic.cpp
@@ -17,5 +17,23 @@ class TaskManager {
         head = nullptr;
     }
 
+    void addTask(std::string desc, int prio) {
+        Task* newTask = new Task();
+        newTask->description = desc;
+        newTask->priority = prio;
+        newTask->next = head;
+        head = newTask;
+
+    }
+
+    void displayTasks() {
+
+        Task* current = head;
+        while (current != nullptr) {
+            cout << "Task: " << current->description << ", Priority: " << current->priority << endl;
+            current = current->next;
+        }
+    }
+
 };
 
