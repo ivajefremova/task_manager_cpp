d88b272fd3762f5c0d45fef87e8cdb24039fecb0
Iva Jefremova <jefremovaiva17@gmail.com>
2026-03-13 17:30:57 +0100

    added comments and changed ->, changed found to current (more readable)

--- logic.cpp
+++ logic.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Task {
     public:
-    std::string description;
+    string description;
     int priority;
     Task* next;
 };
@@ -17,12 +17,12 @@ class TaskManager {
         head = nullptr;
     }
 
-    void addTask(std::string desc, int prio) {
+    void addTask(string desc, int prio) {
         Task* newTask = new Task();
-        newTask->description = desc;
-        newTask->priority = prio;
-        newTask->next = head;
-        head = newTask;
+        (*newTask).description = desc;   //set the description of the new task
+        (*newTask).priority = prio;         //set the priority of the new task
+        (*newTask).next = head;        //point the next of the new task to the current head (basically going down the list)
+        head = newTask;           //then set the head to the new task (the new task is now the first element in the list)
 
     }
 
@@ -30,10 +30,37 @@ class TaskManager {
 
         Task* current = head;
         while (current != nullptr) {
-            cout << "Task: " << current->description << ", Priority: " << current->priority << endl;
-            current = current->next;
+            cout << "task: " << (*current).description << ", priority: " << (*current).priority << endl;
+            current = (*current).next;         //gets the address of the next task and sets it as the current task
+
         }
     }
 
-};
+    void deleteTasks(string target){
 
+        Task* current = head;           //current is a pointer to the address of the pointer head (the first element in the list)
+        Task* prev = nullptr;
+        while (current != nullptr && (*current).description != target) {
+            prev = current;            //set the previous to the current task
+            current = (*current).next;      //set the current to the next task (the address of the next task) 
+        }
+
+        if (current == nullptr) {             //reached the end of the list without finding the target task
+            cout << "Task not found." << endl;
+            return;
+        }
+
+        if (prev == nullptr) {       //we are at the top of the list, here is the target 
+            head = (*current).next;       // set the top to be the next 
+        } 
+        else {
+            (*prev).next = (*current).next;   // we are in the middle so set the next of the prev to be the next of the current
+        }
+
+        delete current;              
+        cout << "'" << target << "' deleted successfully." << endl;
+
+    }
+
+    
+};
