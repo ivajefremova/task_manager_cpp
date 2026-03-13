e9a792a6393766144fe446b43443901645a8f064
Iva Jefremova <jefremovaiva17@gmail.com>
2026-03-13 18:03:27 +0100

    cleared a mistake with cin and getline

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
     int number_of_tasks;
     cout << "enter the number of tasks you would like to manage: ";
     cin >> number_of_tasks;
-
+    cin.ignore(); // clear the input buffer
 
     string task_description;
     int priority;
@@ -16,10 +16,11 @@ int main(){
 
     for (int i = 0; i < number_of_tasks; i++) {
         cout << "enter task description: ";
-        cin >> task_description;
+        getline(cin, task_description);
         cout << "enter task priority: ";
         cin >> priority;
-   
+        cin.ignore(); // clear the input buffer
+
         instance.addTask(task_description, priority);
     }
     
